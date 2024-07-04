8e0e68b212466983b7bd9f3021187760bdd1a739
daniyalirfan1122 <mdaniyal.226688@gmail.com>
2024-07-03 23:21:30 -0700

    form

---
+++ Js.Assignment.9/app.js
@@ -0,0 +1,106 @@
+// ==== Q1 =====
+
+var word = []
+
+// ==== Q2 ====
+
+var word2 = []
+
+
+// ===== Q3 =====
+
+var word3 = ['daniyal', 'hassan', 'adil', 'ahmed']
+
+// ===== Q4 =====
+
+var word4 = [10, 20, 30, 40, 50]
+
+// ===== Q5 =====
+
+var word5 = [true, false, ]
+
+// ===== Q6 =====
+
+var word6 = [1, 5, 10, 'ali', 'ahmed', 'bilal', true, false]
+
+// ===== Q7 =====
+
+var Qualification = ['SSC', 'HSC', 'BSC', 'BS', 'BCOM', 'MS', 'M.Phil', 'PHD']
+
+for (var i = 0; i <Qualification.length; i++){
+    document.write (  i+1 +' ) ' + Qualification[i]+  '<br>')
+}
+
+//===== Q8 =====
+
+var student = ['Micheal', 'John', 'Tony']
+var Scores = [320, 230, 480]
+
+document.write (`<br>Scoore of Michael is ${320}. Percentage: ${64}% <br>`)
+document.write (`Scoore of John is ${230}. Percentage: ${46}% <br>`)
+document.write (`Scoore of Tony is ${480}. Percentage: ${96}% <br>`)
+
+
+//===== Q9 =====
+
+var color = ['blue', 'green', 'yellow', 'red']
+
+document.write ('<br>' +color)
+
+color[4] ='yellowgreen'
+document.write ('<br>' +color)
+
+  color.push('black','Orange')
+document.write ('<br>' +color)
+
+
+color.unshift('white','Mehroon')
+
+document.write ('<br>' +color)
+
+color.shift('white')
+document.write ('<br>' +color)
+
+color.pop('Orange')
+document.write ('<br>' +color)
+
+color2 = color.splice (3,0,'gray')
+
+document.write('<br>'+ color)
+
+color3 = color.splice(5 , 1, 'darkgray')
+document.write ('<br>'+color)
+document.write ('<br>'+color3)
+
+//===== Q10 =====
+
+var Score = [320, 230, 480, 120]
+
+for (var i = 0; i <Score.length; i++){
+   
+}
+
+//===== Q11 =====
+
+var cities = ['karachi','lahore','islamabad','Quetta','peshawar']
+
+cities2 =cities.slice(2,4)
+document.write('<br>'+ cities2 + '<br>')
+
+// ===== Q12 =====
+
+var arr = ['This','is', 'a', 'cat']
+
+document.write('<br>'+ arr + '<br>')
+
+// ==== Q13 ====
+
+var devices = ['keyboard','mouse','printer','monitor']
+
+for (var i = 0; i <devices.length ; i++){
+      document.write('<br>' +devices[i],)
+}
+
+// ==== Q14 ====
+
+
